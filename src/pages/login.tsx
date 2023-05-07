7d9da852450d1314eaeeab66dd3d49fd48e2c1c7
vohizy <hei.vohizy@gmail.com>
2023-05-07 09:50:46 +0300

    feat: login Page

---
+++ src/pages/login.tsx
@@ -0,0 +1,59 @@
+import { Form, Button, Container } from "react-bootstrap";
+import style from "@/styles/Style.module.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
+import { useRouter } from "next/router";
+export default function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+
+  let Submit = () => {
+    router.push("chat");
+    localStorage.setItem("email", email);
+    localStorage.setItem("password", password);
+  };
+
+  return (
+    <>
+      <Container className="d-flex justify-content-center p-4">
+        <div
+          className={`${style.container_form} d-flex align-items-center justify-content-center $purple-400`}
+        >
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Enter email"
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+              />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+              <Form.Check type="checkbox" label="Check me out" />
+            </Form.Group>
+            <Button variant="primary" type="submit" onClick={Submit}>
+              Submit
+            </Button>
+          </Form>
+        </div>
+      </Container>
+    </>
+  );
+}
