68a63e6a8ed1c720a0d41a11d8abecc14174c8d9
vohizy <hei.vohizy@gmail.com>
2023-05-09 09:09:52 +0300

    feat: button logout

--- src/pages/chat.tsx
+++ src/pages/chat.tsx
@@ -1,15 +1,41 @@
-import { useState } from "react";
-import { Container } from "react-bootstrap";
+import { useEffect, useState } from "react";
+import { Container, Button } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@/styles/Style.module.css";
+import { useRouter } from "next/router";
 export default function Chat() {
+  const [name, setName] = useState("");
+  const route = useRouter();
+  const signOut = () => {
+    route.push("/login");
+  };
+  useEffect(() => {
+    const user = localStorage.getItem("user");
+    if (typeof user !== undefined) {
+      setName(() => {
+        if (user) {
+          const value = JSON.parse(user);
+
+          return value.emails;
+        }
+      });
+    }
+  }, []);
   return (
     <>
       <Container className="p-2">
-        <nav className="nav bg-light">
-          <a className="nav-link active" aria-current="page" href="#">
-            Welcome
+        <nav className="nav bg-light d-flex justify-content-between">
+          <a
+            className="nav-link active d-flex justify-content-between"
+            aria-current="page"
+            href="#"
+          >
+            Welcome {name}
           </a>
+          <Button variant="secondary" className="mx-2" onClick={signOut}>
+            Sign out
+          </Button>
+          {""}
         </nav>
         <div className="chat"></div>
       </Container>
