738df399aff806e45d99b54774b1b22cd72df168
vohizy <hei.vohizy@gmail.com>
2023-06-10 06:02:01 +0300

    chore: update

---
+++ src/pages/message/[userId].tsx
@@ -0,0 +1,7 @@
+import { useRouter } from "next/router";
+
+export default function UserId() {
+  const router = useRouter();
+  const id = router.query.channelId;
+  return <h1>your are in user id = {id}</h1>;
+}
